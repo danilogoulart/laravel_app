9df8d8a541d828bbf3eafbd0ad4c0316bb7c70e4
danilogoulart <danilogoulart@outlook.com.br>
2018-03-12 02:33:24 -0300

    Add component topo vue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,34 +15,9 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
+        <topo logo-title="{{ config('app.name', 'Laravel') }}" url-logo="{{ url('/') }}">
+        <!-- Authentication Links -->
+        @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
@@ -66,10 +41,7 @@
                                 </ul>
                             </li>
                         @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        </topo>
 
         @yield('content')
     </div>
